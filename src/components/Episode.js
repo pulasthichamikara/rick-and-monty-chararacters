771794521e70c3bdbe11bfd528b53d4647031826
pcchamikara <pcchamikara@gmail.com>
2022-09-30 13:13:37 +0530

    Init project and created characters

---
+++ src/components/Episode.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Episode({ url }) {
+  return <div>{url}</div>;
+}
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getCaractersRequset = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const resposeJson = await response.json();
+      setData(resposeJson);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCaractersRequset();
+  }, []);
+
+  return { data, loading, error };
+}
+export default useFetch;
